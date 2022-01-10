5347dce64e26604cf4f91e09da627d1f28c2de43
PhelypeOleinik <phelype.oleinik@latex-project.org>
2022-01-10 16:12:34 -0300

    Add l3build script

---
+++ build.lua
@@ -0,0 +1,18 @@
+#!/usr/bin/env texlua
+
+module = "sillypage"
+
+sourcefiles   = { "*.dtx", "*.ins", "sillywalk-map.png" }
+
+installfiles  = { "sillywalk-map.png" }
+
+typesetfiles  = { "*.dtx", "*.tex" }
+
+checkruns     = 1
+typesetruns   = 3
+
+checkengines  = { "pdftex" }
+
+if not release_date then
+   dofile(kpse.lookup("l3build.lua"))
+end
